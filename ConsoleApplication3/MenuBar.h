db4d95d60a7990f3cf91ac8a1c3d6ca4898922c0
Bobby <bobbyvladow@gmail.com>
2020-04-25 01:55:46 +0300

    initial commit

---
+++ ConsoleApplication3/MenuBar.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include  "MenuItem.h"
+
+class MenuBar
+{
+public:
+		MenuBar();
+		~MenuBar();
+
+
+
+private:
+		MenuItem* items;
+};
+
+MenuBar::MenuBar()
+{
+}
+
+MenuBar::~MenuBar()
+{
+}
