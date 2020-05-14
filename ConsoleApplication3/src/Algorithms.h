5005d3f00e4ab7358fda010139ca24546eab3b68
Bobby <bobbyvladow@gmail.com>
2020-05-14 22:02:33 +0300

    New source to keep common functions/algorithms

---
+++ ConsoleApplication3/src/Algorithms.h
@@ -0,0 +1,13 @@
+#ifndef H_ALGORITHMS_H
+#define H_ALGORITHMS_H
+
+#include "SFML/Graphics.hpp"
+#define squared(a) a*a
+
+sf::Vector2f getRectangleCenter(const sf::RectangleShape& rect);
+void BresenhamLine(sf::Vector2i a, sf::Vector2i b, sf::Color color, sf::Image& image);
+float getDistance(const sf::RectangleShape& a, const sf::RectangleShape& b);
+void floodFill(const sf::Vector2i pos, sf::Color color, sf::Image& image, sf::Vector2u lowBounds, sf::Vector2u highBounds);
+void floodFill_4(int x, int y, sf::Color color, sf::Image& image, sf::Vector2u lowBounds, sf::Vector2u highBounds);
+
+#endif //!H_ALGORITHMS_H
